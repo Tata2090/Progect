8988a53b6a745ea555a830bc98de1cb0f49ab310
Tatiana Bykovskih <114161336+Tata2090@users.noreply.github.com>
2022-10-19 16:21:43 +0300

    Revert "Revert "start_files""
    This reverts commit d5a8915633ebd52a9412019ed53ad5c71771d086.

---
+++ auth/users/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import UserSerializer
+from rest_framework.response import Response
+from .models import User
+from rest_framework.exceptions import AuthenticationFailed
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed('Пользователь не найден')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Неверный пароль')
+
+        return Response({
+            'message': 'Успешно'
+        })
